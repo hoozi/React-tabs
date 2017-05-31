d79e63c9dce933cc9e6a2ef6362bf2f3cf76f4a4
AngryPanda <287036406@qq.com>
2017-05-31 09:10:53 +0800

    init

---
+++ tabs.js
@@ -0,0 +1,26 @@
+const Component = React.Component;
+
+class Tabs extends Component {
+
+}
+
+class TabNav extends Component {
+
+}
+
+class TabContent extends Component {
+
+}
+
+class TabPanel extends Component {
+
+}
+
+const tabs = <Tabs>
+                <TabNav>
+                    
+                </TabNav>
+                <TabContent>
+                    <TabPanel></TabPanel>
+                </TabContent>
+             </Tabs>
